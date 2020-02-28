019cf2b2fb4a33f71106b6108afca79f7d1cbb30
kevin sebastian <kevinseb16@gmail.com>
2020-02-28 17:56:51 +0530

    Added Geofire and successfully migrated the store operation to firestore db from realtime:P..Major update

--- App/app/src/main/java/com/marqur/android/Marker.java
+++ App/app/src/main/java/com/marqur/android/Marker.java
@@ -1,5 +1,7 @@
 package com.marqur.android;
 
+import com.google.firebase.firestore.GeoPoint;
+
 public class Marker {
 
 
@@ -7,13 +9,13 @@ public class Marker {
     public String author;
     public String date_created;
     public String date_modified;
-
+    public GeoPoint location;
     public int views;
     public int upvotes;
     public int downvotes;
     public int comments_count;
     public int reports;
-    public Content contents;
+
 
 
     public Marker() {
@@ -21,18 +23,18 @@ public class Marker {
     }
 
 
-    public Marker(String title, String author, String date_created, String date_modified, int views, int upvotes, int downvotes, int comments_count, int reports, Content contents) {
+    public Marker(String title, String author,GeoPoint location, String date_created, String date_modified, int views, int upvotes, int downvotes, int comments_count, int reports) {
 
         this.title = title;
         this.author = author;
         this.date_created = date_created;
         this.date_modified = date_modified;
-
+        this.location=location;
         this.views = views;
         this.upvotes = upvotes;
         this.downvotes = downvotes;
         this.comments_count = comments_count;
         this.reports = reports;
-        this.contents = contents;
+
     }
 }
